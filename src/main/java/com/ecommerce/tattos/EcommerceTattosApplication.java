8f5690dbcb303efa9278e31e021ed37659aa6da0
Jean Lozano <michecoanexa52@gmail.com>
2022-05-02 17:52:31 -0500

    Initial commit

---
+++ src/main/java/com/ecommerce/tattos/EcommerceTattosApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tattos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceTattosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceTattosApplication.class, args);
+    }
+
+}
